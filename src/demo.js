535af421ee30cafcdca8bce8366720e31352a350
xintianle <20307110196@fudan.edu.cn>
2023-04-01 16:43:47 +0800

    read multi vertex and draw

--- src/demo.js
+++ src/demo.js
@@ -2,6 +2,21 @@ function xyConvert(canvasPos, canvasSize){
     return [2 * canvasPos[0]/canvasSize.maxX - 1, (-2) * canvasPos[1]/canvasSize.maxY + 1, canvasPos[2]];
 }
 
+// Vertex shader program
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'attribute float a_PointSize;\n' + // attribute variable
+    'void main() {\n' +
+    '  gl_Position = a_Position;\n' +
+    '  gl_PointSize = a_PointSize;\n' +
+    '}\n';
+
+// Fragment shader program
+var FSHADER_SOURCE =
+    'void main() {\n' +
+    '  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\n' +
+    '}\n';
+
 function main(){
     var webgl = document.getElementById("webgl");
     var gl = getWebGLContext(webgl);
@@ -9,20 +24,6 @@ function main(){
         console.log('Failed to get the rendering context for WebGL');
         return;
     }
-    // Vertex shader program
-    var VSHADER_SOURCE =
-        'attribute vec4 a_Position;\n' + // attribute variable
-        'void main() {\n' +
-        '  gl_Position = a_Position;\n' +
-        '  gl_PointSize = 10.0;\n' +
-        '}\n';
-
-// Fragment shader program
-    var FSHADER_SOURCE =
-        'void main() {\n' +
-        '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
-        '}\n';
-
     // Initialize shaders
     if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
         console.log('Failed to intialize shaders.');
@@ -31,19 +32,61 @@ function main(){
 
     // Get the storage location of a_Position
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
     if (a_Position < 0) {
         console.log('Failed to get the storage location of a_Position');
         return;
     }
 
-    // console.log(canvasSize);
-    for (pos of vertex_pos){
-        // console.log(pos);
-        // console.log(xyConvert(pos, canvasSize));
-        // Pass vertex position to attribute variable
-        gl.vertexAttrib3fv(a_Position, xyConvert(pos, canvasSize));
-// Draw
-        gl.drawArrays(gl.POINTS, 0, 1);
+    // Write the positions of vertices to a vertex shader
+    var n = initVertexBuffers(gl, vertex_pos);
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return;
     }
 
+    // Specify the color for clearing <canvas>
+    gl.clearColor(0, 0, 0, 1);
+
+    // Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Draw three points
+    gl.drawArrays(gl.POINTS, 0, n);
+}
+
+
+function initVertexBuffers(gl, vertex_pos) {
+    var arr = [];
+    for (let i in vertex_pos){
+        arr.push(xyConvert(vertex_pos[i], canvasSize)[0]);
+        arr.push(xyConvert(vertex_pos[i], canvasSize)[1]);
+    }
+    var vertices = new Float32Array(arr);
+    var n = vertex_pos.length; // The number of vertices
+
+    // Create a buffer object
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+
+    // Bind the buffer object to target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // Write date into the buffer object
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return -1;
+    }
+    // Assign the buffer object to a_Position variable
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    // Enable the assignment to a_Position variable
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
 }
